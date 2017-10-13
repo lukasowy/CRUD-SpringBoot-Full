c4fba3da7cbbd1bf7b11bcb88d19f8fe21c5aa0c
Lukasowy <lukasowy@gmail.com>
2017-10-13 19:01:44 +0200

    Schedular job

--- src/main/java/com/lukasowy/CrudSpringBootFullApplication.java
+++ src/main/java/com/lukasowy/CrudSpringBootFullApplication.java
@@ -2,8 +2,10 @@ package com.lukasowy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
 public class CrudSpringBootFullApplication {
 
 	public static void main(String[] args) {
