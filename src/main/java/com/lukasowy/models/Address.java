472e2047465da91dd9f748cda294855ab7f05684
Lukasowy <lukasowy@gmail.com>
2017-10-27 20:06:02 +0200

    Server side validation for Address - Advanced(Custom Validator)

--- src/main/java/com/lukasowy/models/Address.java
+++ src/main/java/com/lukasowy/models/Address.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Size;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import com.lukasowy.utils.ConstantUtils;
+import com.lukasowy.validator.Validate;
 
 @Entity
 public class Address extends AbstractPersistable<Long> {
@@ -17,22 +18,19 @@ public class Address extends AbstractPersistable<Long> {
 	private static final long serialVersionUID = 1L;
 	private transient Long id;
 	@NotNull
-	@Size(min = 10, max = 100, message = "Please enter between {min}-{max} characters")
+	@Validate(min = 10, max = 100, regexp = "", message = "Please enter between {min}-{max} characters")
 	private String addressLine;
 	@NotNull
-	@Size(min = 3, max = 15, message = "Please enter between {min}-{max} characters")
-	@Pattern(regexp=ConstantUtils.CHAR_PATTERN, message="Please enter only characters")
+	@Validate(min = 3, max = 15, regexp=ConstantUtils.CHAR_PATTERN,  message = "Please enter between {min}-{max} characters")
 	private String city;
 	@NotNull
-	@Size(min = 3, max = 15, message = "Please enter between {min}-{max} characters")
-	@Pattern(regexp=ConstantUtils.CHAR_PATTERN, message="Please enter only characters")
+	@Validate(min = 3, max = 15, regexp=ConstantUtils.CHAR_PATTERN,  message = "Please enter between {min}-{max} characters")
 	private String state;
 	@NotNull
-	@Size(min = 3, max = 15, message = "Please enter between {min}-{max} characters")
-	@Pattern(regexp=ConstantUtils.CODE_PATTERN, message="Please enter only digits")
+	@Validate(min = 3, max = 15, regexp=ConstantUtils.CHAR_PATTERN,  message = "Please enter between {min}-{max} characters")
 	private String country;
 	@NotNull
-	@Size(min = 6, max = 6, message = "Please enter  at least {min} digits")
+	@Validate(min = 6, max = 6, regexp=ConstantUtils.CODE_PATTERN,  message = "Please enter between {min}-{max} digits")
 	private String pinCode;
 	private transient Long userId;
 
